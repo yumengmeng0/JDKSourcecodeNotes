dcf724973bb567209b6f8d6bc94cdfdd5d539460
YuMengMeng <whoyumengmeng@163.com>
2022-09-08 09:32:34 +0800

    init

---
+++ src/main/java/StringNotes.java
@@ -0,0 +1,14 @@
+/**
+ * java.lang.String 类
+ *
+ * @author: YuMengMeng
+ * @date: 2022/9/7
+ */
+public class StringNotes {
+    private static String string;
+
+    public static void main(String[] args) {
+        string = "hello string";
+        System.out.println("string = " + string);
+    }
+}
